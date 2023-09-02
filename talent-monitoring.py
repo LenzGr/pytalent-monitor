43281eb0e7cc7731aa5616b43a640866c80f41db
Lenz Grimmer <lenz@grimmer.com>
2023-09-02 17:48:36 +0200

    Various cleanups and improvements
    Capitalize TIMEZONE as constant.
    Define function for token refresh.
    Improve variable naming and use of unpacking to make the code more readable.
    Simplify URL building for endpoints.
    Use f-strings for formatting instead of concatenation.
    Remove unnecessary global token statement and make
    use of function parameters to pass data between functions.
    Make use of dictionary indexing to access nested data more
    cleanly.
    
    Signed-off-by: Lenz Grimmer <lenz@grimmer.com>

--- talent-monitoring.py
+++ talent-monitoring.py
@@ -9,8 +9,7 @@ import requests
 logging.basicConfig(level=logging.INFO)
 
 BASE_URL = "https://www.talent-monitoring.com/prod-api"
-timezone = "+02:00"
-token = None
+TIMEZONE = "+02:00"
 
 class AuthenticationError(Exception):
     pass
@@ -32,7 +31,6 @@ def get_credentials():
     raise ValueError("Credentials not provided via command line arguments or environment variables.")
 
 def login(username, password):
-    global token
     login_data = {
         "username": username,
         "password": password
@@ -42,63 +40,70 @@ def login(username, password):
     if "token" in response_data:
         token = response_data["token"]
         logging.debug("Login successful - received token: %s", token)
+        return token
     else:
         logging.error("Login failed. Got status code %s", response.status_code)
         raise AuthenticationError("Authentication failed")
 
-def get_data(endpoint):
-    global token
+def refresh_token(username, password):
+    logging.debug("Token expired. Refreshing token...")
+    return login(username, password)
+
+def get_data(endpoint, token):
     headers = {
         "Authorization": f"Bearer {token}"
     }
     response = requests.get(f"{BASE_URL}/{endpoint}", headers=headers)
+    
     if response.status_code == 401:  # Unauthorized, token might be expired
-        logging.debug("Token expired. Refreshing token...")
-        login(username="your_username", password="your_password")
+        token = refresh_token(username, password)
         headers["Authorization"] = f"Bearer {token}"
         response = requests.get(f"{BASE_URL}/{endpoint}", headers=headers)
 
     if response.status_code == 200:
         return response.json()
     else:
-        debug.error("Failed to fetch data. Status Code: %s", response.status_code)
+        logging.error("Failed to fetch data. Status Code: %s", response.status_code)
         return None
 
 if __name__ == "__main__":
     username, password = get_credentials()
-    login(username = username, password = password)
+    token = login(username, password)
 
-    data = get_data(endpoint="system/station/list")
+    data = get_data(endpoint="system/station/list", token=token)
     if data:
-        status = data['rows'][0]['status']
+        first_station = data['rows'][0]
+        status = first_station['status']
         print(f"Status: {status}")
-        stationName = data['rows'][0]['stationName']
+        stationName = first_station['stationName']
         print(f"Station Name: {stationName}")
-        powerStationGuid = data['rows'][0]['powerStationGuid']
+        powerStationGuid = first_station['powerStationGuid']
         logging.debug("GUID: %s", powerStationGuid)
 
-        data = get_data(endpoint="system/station/getPowerStationByGuid?powerStationGuid="+powerStationGuid+"&timezone="+timezone)
+        data = get_data(endpoint=f"system/station/getPowerStationByGuid?powerStationGuid={powerStationGuid}&timezone={TIMEZONE}", token=token)
         if data:
-            totalActivePower = data['data']['totalActivePower']
+            power_data = data['data']
+            totalActivePower = power_data['totalActivePower']
             print(f"Total Active Power (W): {totalActivePower}")
-            dayEnergy = data['data']['dayEnergy']
+            dayEnergy = power_data['dayEnergy']
             print(f"Daily energy (Wh): {dayEnergy}")
-            monthEnergy = data['data']['monthEnergy']
+            monthEnergy = power_data['monthEnergy']
             print(f"Monthly energy (Wh): {monthEnergy}")
-            yearEnergy = data['data']['yearEnergy']
+            yearEnergy = power_data['yearEnergy']
             print(f"Yearly energy (Wh): {yearEnergy}")
-                
-        data = get_data(endpoint="system/station/selectLayoutComponents?powerStationGuid="+powerStationGuid)
+
+        data = get_data(endpoint=f"system/station/selectLayoutComponents?powerStationGuid={powerStationGuid}", token=token)
         if data:
-            pv1Voltage = data['data']['components'][0]['pv1Voltage']
+            components = data['data']['components'][0]
+            pv1Voltage = components['pv1Voltage']
             print(f"Panel1 Voltage (V): {pv1Voltage}")
-            pv1Current = data['data']['components'][0]['pv1Current']
+            pv1Current = components['pv1Current']
             print(f"Panel1 Current (A): {pv1Current}")
-            pv1Power = data['data']['components'][0]['pv1Power']
+            pv1Power = components['pv1Power']
             print(f"Panel1 Power (W): {pv1Power}")
-            pv2Voltage = data['data']['components'][0]['pv2Voltage']
+            pv2Voltage = components['pv2Voltage']
             print(f"Panel2 Voltage (V): {pv2Voltage}")
-            pv2Current = data['data']['components'][0]['pv2Current']
+            pv2Current = components['pv2Current']
             print(f"Panel2 Current (A): {pv2Current}")
-            pv2Power = data['data']['components'][0]['pv2Power']
-            print(f"Panel2 Power (W): {pv2Power}")                    
+            pv2Power = components['pv2Power']
+            print(f"Panel2 Power (W): {pv2Power}")
